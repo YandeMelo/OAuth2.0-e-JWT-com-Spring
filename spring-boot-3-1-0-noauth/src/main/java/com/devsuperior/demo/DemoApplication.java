a88491e4f3aa6be11932037b48c2b9720c96d16b
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2024-04-10 22:40:40 -0300

    Atualizando a Main para a documentação com Swagger

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package com.devsuperior.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Spring Boot Auth", version = "1.0", description = "API desenvolvida para o aprendizado de Spring Security e Swagger"))
 @SpringBootApplication
 public class DemoApplication {
 
